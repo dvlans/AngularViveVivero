2b36506ddb72f971b7185c06f5ee84d8502d3ec4
Diego Emilio Fuentes Gomez <fuentesdiego054@gmail.com>
2019-01-16 21:33:06 -0300

    agregar footer imagenes redes sociales otras , css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import {ContactoComponent} from './Components/contacto/contacto.component';
+import {FooterComponent} from './Components/footer/footer.component';
+import {M2o2Component} from './Components/m2o2/m2o2.component';
+import {ProyectosComponent} from './Components/proyectos/proyectos.component';
+import {SomosComponent} from './Components/somos/somos.component';
+import {TiendaComponent} from './Components/tienda/tienda.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContactoComponent,
+    FooterComponent,
+    M2o2Component,
+    ProyectosComponent,
+    SomosComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule
